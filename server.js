dfc2ea00112f0d1907d5656d234d38f254f51b89
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2025-12-05 15:11:40 -0500

    A Basic Server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("We have a package.json");
+import express from "express";
+
+const PORT = 8000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
